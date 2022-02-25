4b2fbbdd97ac6fd9513b2ff5da1e5a9caee83980
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-02-24 16:47:39 -0800

    whitespace: Convert tabs to spaces

--- src/test/java/com/csc366/project/IngredientTest.java
+++ src/test/java/com/csc366/project/IngredientTest.java
@@ -22,62 +22,62 @@ import org.springframework.test.context.TestPropertySource;
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(properties = {
-	"spring.main.banner-mode=off",
-	"spring.jpa.hibernate.ddl-auto=update",
-	"logging.level.root=ERROR",
-	"logging.level.csc366=DEBUG",
-	"logging.level.org.hibernate.SQL=DEBUG",
-	"logging.level.org.hibernate.type.descriptor.sql.BasicBinder=TRACE", // display prepared statement parameters
-	"spring.jpa.properties.hibernate.format_sql=true",
-	"spring.jpa.show-sql=false",   // prevent duplicate logging
-	"spring.jpa.properties.hibernate.show_sql=false",
-	"logging.pattern.console= %d{yyyy-MM-dd HH:mm:ss} - %msg%n"
+    "spring.main.banner-mode=off",
+    "spring.jpa.hibernate.ddl-auto=update",
+    "logging.level.root=ERROR",
+    "logging.level.csc366=DEBUG",
+    "logging.level.org.hibernate.SQL=DEBUG",
+    "logging.level.org.hibernate.type.descriptor.sql.BasicBinder=TRACE", // display prepared statement parameters
+    "spring.jpa.properties.hibernate.format_sql=true",
+    "spring.jpa.show-sql=false",   // prevent duplicate logging
+    "spring.jpa.properties.hibernate.show_sql=false",
+    "logging.pattern.console= %d{yyyy-MM-dd HH:mm:ss} - %msg%n"
 })
 class IngredientTest {
 
-	private final static Logger log = LoggerFactory.getLogger(IngredientTest.class);
+    private final static Logger log = LoggerFactory.getLogger(IngredientTest.class);
 
-	@Autowired
-	private IngredientRepository ingredientRepository;
+    @Autowired
+    private IngredientRepository ingredientRepository;
 
-	private final Ingredient ingredient = new Ingredient("test");
+    private final Ingredient ingredient = new Ingredient("test");
 
-	@BeforeEach
+    @BeforeEach
     private void setup() {
-		ingredientRepository.saveAndFlush(ingredient);
+        ingredientRepository.saveAndFlush(ingredient);
     }
 
-	@Test
-	public void testSaveIngredient() {
-		Ingredient actual = ingredientRepository.findByName("test");
+    @Test
+    public void testSaveIngredient() {
+        Ingredient actual = ingredientRepository.findByName("test");
 
-		log.info(actual.toString());
+        log.info(actual.toString());
 
-		assertNotNull(actual);
-		assertEquals(ingredient.getName(), actual.getName());
-	}
+        assertNotNull(actual);
+        assertEquals(ingredient.getName(), actual.getName());
+    }
 
-	@Test
+    @Test
     public void testDeletePerson() {
-		ingredientRepository.delete(ingredient);
-		ingredientRepository.flush();
+        ingredientRepository.delete(ingredient);
+        ingredientRepository.flush();
     }
 
-	@Test
+    @Test
     public void testFindAllPersons() {
-		assertNotNull(ingredientRepository.findAll());
+        assertNotNull(ingredientRepository.findAll());
     }
 
-	@Test
+    @Test
     public void testDeletByPersonId() {
-		Ingredient actual = ingredientRepository.findByName("test");
-		ingredientRepository.deleteById(actual.getName());
-		ingredientRepository.flush();
+        Ingredient actual = ingredientRepository.findByName("test");
+        ingredientRepository.deleteById(actual.getName());
+        ingredientRepository.flush();
     }
 
     @Test
     public void testJpqlFinder() {
-		Ingredient actual = ingredientRepository.findByName("test");
-		assertEquals(actual.getName(), ingredient.getName());
+        Ingredient actual = ingredientRepository.findByName("test");
+        assertEquals(actual.getName(), ingredient.getName());
     }
 }
